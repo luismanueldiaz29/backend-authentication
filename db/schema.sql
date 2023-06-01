18be0b3b9db8b13304ad88443d3056196a8a5447
Luis Manuel Diaz <luis.diaz@fsteam.co>
2023-05-31 20:32:16 -0500

    Se agregó el script de la tabla usuarios del proyecto fruitvalle

--- db/schema.sql
+++ db/schema.sql
@@ -13,3 +13,17 @@ create table users
 -- USER -> admin@admin PASS -> 123
 INSERT INTO users(NAME, SECOND_NAME, LAST_NAME, USERNAME, PASSWORD)
 VALUES ('admin', null, 'admin', 'admin@admin','d404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db');
+
+----------------------------------------------
+
+create table usuarios
+(
+    usuario_id   bigserial,
+    usuario      varchar(255) not null,
+    clave        varchar(255) not null,
+    tipo_usuario varchar(50)  not null,
+    email        varchar(255)
+);
+
+insert into usuarios ( usuario, clave, tipo_usuario, email)
+values ('admin@admin','d404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db','CLIENTE','admin@gmail.com')
